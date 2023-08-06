fa14b8c1987a79c7bbc904337e4f81dbbf2ef0e4
Titapon Keawbua <64160047@go.buu.ac.th>
2023-08-07 00:15:55 +0700

    set project git

---
+++ oxGUI1/src/main/java/com/mycompany/oxgui1/OxGUI1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oxgui1;
+
+/**
+ *
+ * @author minnie
+ */
+public class OxGUI1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
